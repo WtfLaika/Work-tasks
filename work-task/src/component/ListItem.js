edfdd43f15eedaa8c754e3e77c625ce9f1152bdd
Denys Shmahailo <omgdoni@gmail.com>
2021-08-16 14:28:40 +0300

    delete work-task

--- work-task/src/component/ListItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ListItem({result}) {
-    return (
-        <div className='list-item' style={{backgroundColor:result.color}}>
-            {result.str}
-        </div>
-    )
-}
